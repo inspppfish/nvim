8d375aba4b1609d6dfde673446f44bbb801210c8
ysyx_22050977-ZhouZijie <whenyueqi@gmail.com>
2023-02-25 03:22:43 +0800

    append

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -107,4 +107,23 @@ pluginKeys.telescopeList = {
 	},
 }
 
+pluginKeys.mapLSP = function(mapbuf)
+	-- 这些都不太好用，有待重写
+	-- rename
+--	mapbuf("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opt)
+--	-- code action
+--	mapbuf("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opt)
+	-- go xx
+	mapbuf("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
+--	mapbuf("n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
+--	mapbuf("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opt)
+--	mapbuf("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
+--	mapbuf("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
+--	-- diagnostic
+--	mapbuf("n", "gp", "<cmd>lua vim.diagnostic.open_float()<CR>", opt)
+--	mapbuf("n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opt)
+--	mapbuf("n", "gj", "<cmd>lua vim.diagnostic.goto_next()<CR>", opt)
+--	mapbuf("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opt)
+end
+
 return pluginKeys
---
+++ lua/lsp/setup.lua
@@ -0,0 +1,9 @@
+local lsp_installer = require("nvim-lsp-installer")
+
+-- 安装列表
+-- { key: 语言 value: 配置文件 }
+-- key 必须为下列网址列出的名称
+-- https://github.com/williamboman/nvim-lsp-installer#available-lsps
+local servers = {
+	sumneko_lua = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
+}
